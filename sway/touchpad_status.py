6207250784d22ff44aac9e2d4cafbc8d0ac6e39d
testpoo <402861037@163.com>
2025-02-19 23:17:05 +0800

    更新配置

--- sway/touchpad_status.py
+++ sway/touchpad_status.py
@@ -7,6 +7,6 @@ lists = json.load(os.popen("swaymsg -t get_inputs"))
 temp = [li for li in lists if li['identifier'] == '2362:597:SYNA3602:00_093A:0255_Touchpad']
 status = temp[0]['libinput']['send_events']
 if status == "enabled":
-    os.system("swaymsg input \"2362:597:SYNA3602:00_093A:0255_Touchpad\" events disabled")
+    print("开")
 else:
-    os.system("swaymsg input \"2362:597:SYNA3602:00_093A:0255_Touchpad\" events enabled")
+    print("关")
